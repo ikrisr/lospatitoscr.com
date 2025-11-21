647cda7f677b4e495406a2103280827a7c6bff12
Krisley Castro <kriizcastro@hotmail.com>
2025-11-21 12:04:38 -0600

    Implementacion de la MVC

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,12 @@
+</div>
+<footer class="bg-light text-center text-lg-start mt-5 py-3 border-top">
+    <div class="container">
+        <p class="mb-0 text-muted">Las Kris |Los patitos SA &copy; 2025</p>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
