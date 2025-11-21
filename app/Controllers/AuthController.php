512ad19c54220a82f6e4d65059faca604bfe1ad8
Krisley Castro <kriizcastro@hotmail.com>
2025-11-21 15:03:50 -0600

    Correccion de rutas, y implementacion de dashboard usuarios

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php // ESTO ES POR MIENTRAS KRISTEL TERMINA EL LOGIN
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+
+class AuthController extends Controller
+{
+    public function loginForm()
+    {
+        echo "Login temporal...";
+    }
+
+    public function login()
+    {
+        echo "Procesando login temporal...";
+    }
+
+    public function logout()
+    {
+        echo "Logout temporal...";
+    }
+
+    public function dashboard()
+    {
+        echo "Dashboard temporal...";
+    }
+}
